c74bbadf25034ff8d09f3d2b17ad17eafa55ca55
wangsungtaek <tjdxorv@naver.com>
2024-03-19 00:30:27 +0900

    Implemented WordRelay functionality using a class structure

--- 2.끝말잇기/WordRelay.jsx
+++ 2.끝말잇기/WordRelay.jsx
@@ -10,21 +10,47 @@ class wordRelay extends React.Component {
     }
   };
 
-  wordChange(e) {
-    console.log(e);
+  wordChange = (e) => {
+    this.setState({
+      value: e.target.value
+    })
   }
 
-  onSubmit(e) {
+  onSubmit = (e) => {
     e.preventDefault();
-    console.log(11);
+    
+    if(this.state.value.length <= 0) return;
+    const firstWord = this.state.value.charAt(0);
+    const answer = this.state.word.slice(-1);
+
+    if(firstWord == answer) {
+      this.setState({
+        value: "",
+        result: "정답입니다.",
+        word: this.state.value
+      })
+    } else {
+      this.setState({
+        value: "",
+        result: "오답입니다."
+      })
+    }
+
+    this.input.focus();
   }
+
+  inputRef = (c) => {
+    this.input = c;
+  }
+
+  input;
 
   render() {
     return (
       <>
         <div>{this.state.word}</div>
         <form onSubmit={this.onSubmit}>
-          <input onChange={this.wordChange} />
+          <input ref={this.inputRef} value={this.state.value} onChange={this.wordChange} />
           <button type="summit">제출</button>
         </form>
         <div>{this.state.result}</div>
